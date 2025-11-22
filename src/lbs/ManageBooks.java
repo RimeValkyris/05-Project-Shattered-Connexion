170f2ded27c77776f4f3ee1850fe959d9e9e8afd
YaranaikaKiryu <146524104+YaranaikaKiryu@users.noreply.github.com>
2025-11-22 14:52:17 +0800

    Added UIs

--- src/lbs/ManageBooks.java
+++ src/lbs/ManageBooks.java
@@ -11,6 +11,8 @@ import java.awt.Cursor;
 import java.awt.Font;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ManageBooks extends JFrame {
 
@@ -19,6 +21,9 @@ public class ManageBooks extends JFrame {
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
 
 	/**
 	 * Launch the application.
@@ -135,35 +140,123 @@ public class ManageBooks extends JFrame {
 		JLabel lblNewLabel_1 = new JLabel("ISBN");
 		lblNewLabel_1.setForeground(Color.WHITE);
 		lblNewLabel_1.setFont(new Font("Segoe UI Symbol", Font.BOLD, 16));
-		lblNewLabel_1.setBounds(10, 118, 56, 20);
+		lblNewLabel_1.setBounds(10, 116, 56, 20);
 		panel_1.add(lblNewLabel_1);
 		
 		textField = new JTextField();
-		textField.setBounds(97, 118, 257, 20);
+		textField.setBounds(97, 119, 257, 20);
 		panel_1.add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblNewLabel_1_1 = new JLabel("Book Title");
 		lblNewLabel_1_1.setForeground(Color.WHITE);
 		lblNewLabel_1_1.setFont(new Font("Segoe UI Symbol", Font.BOLD, 16));
-		lblNewLabel_1_1.setBounds(10, 170, 83, 20);
+		lblNewLabel_1_1.setBounds(10, 167, 83, 20);
 		panel_1.add(lblNewLabel_1_1);
 		
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
-		textField_1.setBounds(97, 173, 257, 20);
+		textField_1.setBounds(97, 170, 257, 20);
 		panel_1.add(textField_1);
 		
 		JLabel lblNewLabel_1_1_1 = new JLabel("Author");
 		lblNewLabel_1_1_1.setForeground(Color.WHITE);
 		lblNewLabel_1_1_1.setFont(new Font("Segoe UI Symbol", Font.BOLD, 16));
-		lblNewLabel_1_1_1.setBounds(10, 222, 83, 20);
+		lblNewLabel_1_1_1.setBounds(10, 216, 56, 20);
 		panel_1.add(lblNewLabel_1_1_1);
 		
 		textField_2 = new JTextField();
 		textField_2.setColumns(10);
-		textField_2.setBounds(97, 225, 257, 20);
+		textField_2.setBounds(97, 219, 257, 20);
 		panel_1.add(textField_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(97, 275, 257, 20);
+		panel_1.add(textField_3);
+		
+		JLabel lblNewLabel_1_1_1_1 = new JLabel("Genre");
+		lblNewLabel_1_1_1_1.setForeground(Color.WHITE);
+		lblNewLabel_1_1_1_1.setFont(new Font("Segoe UI Symbol", Font.BOLD, 16));
+		lblNewLabel_1_1_1_1.setBounds(10, 271, 83, 23);
+		panel_1.add(lblNewLabel_1_1_1_1);
+		
+		JLabel lblNewLabel_1_1_1_1_1 = new JLabel("Year");
+		lblNewLabel_1_1_1_1_1.setForeground(Color.WHITE);
+		lblNewLabel_1_1_1_1_1.setFont(new Font("Segoe UI Symbol", Font.BOLD, 16));
+		lblNewLabel_1_1_1_1_1.setBounds(10, 371, 83, 20);
+		panel_1.add(lblNewLabel_1_1_1_1_1);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(97, 322, 257, 20);
+		panel_1.add(textField_4);
+		
+		JLabel lblNewLabel_1_1_1_2 = new JLabel("Publisher");
+		lblNewLabel_1_1_1_2.setForeground(Color.WHITE);
+		lblNewLabel_1_1_1_2.setFont(new Font("Segoe UI Symbol", Font.BOLD, 16));
+		lblNewLabel_1_1_1_2.setBounds(10, 322, 83, 20);
+		panel_1.add(lblNewLabel_1_1_1_2);
+		
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+		textField_5.setBounds(97, 374, 257, 20);
+		panel_1.add(textField_5);
+		
+		JButton btnAdd = new JButton("Add");
+		btnAdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnAdd.setOpaque(true);
+		btnAdd.setForeground(Color.WHITE);
+		btnAdd.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		btnAdd.setFocusPainted(false);
+		btnAdd.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnAdd.setBorderPainted(false);
+		btnAdd.setBackground(new Color(55, 65, 81));
+		btnAdd.setBounds(30, 439, 74, 40);
+		panel_1.add(btnAdd);
+		
+		JButton btnDelete = new JButton("Delete");
+		btnDelete.setOpaque(true);
+		btnDelete.setForeground(Color.WHITE);
+		btnDelete.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		btnDelete.setFocusPainted(false);
+		btnDelete.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnDelete.setBorderPainted(false);
+		btnDelete.setBackground(new Color(55, 65, 81));
+		btnDelete.setBounds(231, 439, 74, 40);
+		panel_1.add(btnDelete);
+		
+		JButton btnUpdate = new JButton("Update");
+		btnUpdate.setOpaque(true);
+		btnUpdate.setForeground(Color.WHITE);
+		btnUpdate.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		btnUpdate.setFocusPainted(false);
+		btnUpdate.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnUpdate.setBorderPainted(false);
+		btnUpdate.setBackground(new Color(55, 65, 81));
+		btnUpdate.setBounds(125, 439, 89, 40);
+		panel_1.add(btnUpdate);
+		
+		JButton btnGoBack = new JButton("Go Back");
+		btnGoBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				LibraryDashboard dashboard = new LibraryDashboard();
+				dashboard.setVisible(true);
+				dispose();
+			}
+		});
+		btnGoBack.setOpaque(true);
+		btnGoBack.setForeground(Color.WHITE);
+		btnGoBack.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		btnGoBack.setFocusPainted(false);
+		btnGoBack.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnGoBack.setBorderPainted(false);
+		btnGoBack.setBackground(new Color(55, 65, 81));
+		btnGoBack.setBounds(30, 540, 89, 40);
+		panel_1.add(btnGoBack);
 		
 		JPanel panel_1_1 = new JPanel();
 		panel_1_1.setBackground(new Color(79, 70, 229));
@@ -174,6 +267,11 @@ public class ManageBooks extends JFrame {
 		panel_1_1_1.setBackground(new Color(79, 70, 229));
 		panel_1_1_1.setBounds(0, 0, 1271, 49);
 		contentPane.add(panel_1_1_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Book Management");
+		lblNewLabel_2.setFont(new Font("Segoe UI Symbol", Font.BOLD, 32));
+		lblNewLabel_2.setBounds(698, 60, 291, 49);
+		contentPane.add(lblNewLabel_2);
 
 	}
 }
